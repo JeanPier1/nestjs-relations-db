13f855272a4655d82b559a412251c2ca5037dcbd
JeanPier1 <jeangonza@upeu.edu.pe>
2021-10-19 17:17:21 -0500

    format-01

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { Question } from './question/entities/question.entity';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mariadb',
+      type: 'mysql',
       host: 'localhost',
       port: 3306,
       username: 'root',

4735b15864b5d3e209c18205f1feeeea9b053389
JeanPier1 <jeangonza@upeu.edu.pe>
2021-10-22 18:47:18 -0500

    user&&rol

--- src/rol/entities/rol.entity.ts
+++ src/rol/entities/rol.entity.ts
@@ -1,1 +1,13 @@
-export class Rol {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Rol {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ default: '1', length: '1', type: 'char' })
+  state: string;
+
+  @Column()
+  name: string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ default: '1', length: '1', type: 'char' })
+  state: string;
+
+  @Column()
+  username: string;
+}
